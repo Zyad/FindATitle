1e4f515065ce663e0110c9a2d8f6acc8a7d14941
zyad <big17.z@gmail.com>
2012-04-29 00:54:03 +0200

    2

--- results.php
+++ results.php
@@ -1,7 +1,7 @@
 <?php
 $answer=$_GET[answer];
 $keyword=$_GET[keyword];
-echo($keyword); echo"</BR>";
+//echo($keyword); echo"</BR>";
 require_once dirname(__FILE__).'/../../ech1/lib/EchoNest/Autoloader.php';
 EchoNest_Autoloader::register();
 $echonest = new EchoNest_Client();
@@ -12,7 +12,7 @@ $songApi->setOption('raw', false);
 
 if(( $responses = $songApi->search(array('title' => $answer, 'results' => 25,'sort' => 'song_hotttnesss-desc'))) && (preg_match("/\b".$keyword."\b/i", $answer)) )
 { 
-echo"The title is correct , now find the artist who sings: ".$answer;
+echo"The title is correct, now find the artist who sings: ".$answer;
 ?>
 <form action="results_artist.php" method="GET">
 	<label for="username">artist:</label>
